39912b4b6175b26b839aa9e92988cce7b78e8f21
RupaEnggVille <rupa@enggville.com>
2026-05-14 12:09:50 +0530

    Create db.php

---
+++ app/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "mysql";
+$user = "root";
+$password = "";
+$database = "student_management";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
