b6cab133b578188f13931ba70639078ca88cf63e
RupaEnggVille <rupa@enggville.com>
2026-05-15 13:17:17 +0530

    Update add_student.php

--- app/add_student.php
+++ app/add_student.php
@@ -1,49 +1,58 @@
 <?php
 include 'db.php';
 
-$query = "SELECT * FROM students";
-$result = mysqli_query($conn, $query);
+if(isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $roll_no = $_POST['roll_no'];
+    $subject = $_POST['subject'];
+    $marks = $_POST['marks'];
+    $grade = $_POST['grade'];
+
+    $query = "INSERT INTO students(name, roll_no, subject, marks, grade)
+              VALUES('$name', '$roll_no', '$subject', '$marks', '$grade')";
+
+    if(mysqli_query($conn, $query)) {
+        echo "<script>alert('Student Added Successfully');</script>";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Student Marks Management System</title>
+    <title>Add Student</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div class="container">
-    <h1>Student Marks Management System</h1>
+    <h1>Add Student</h1>
 
-    <div class="nav-buttons">
-        <a href="add_student.php">Add Student</a>
-        <a href="search.php">Search Student</a>
-    </div>
+    <form method="POST">
 
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Roll No</th>
-            <th>Subject</th>
-            <th>Marks</th>
-            <th>Grade</th>
-        </tr>
+        <input type="text" name="name" placeholder="Enter Name" required>
 
-        <?php while($row = mysqli_fetch_assoc($result)) { ?>
-        <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['roll_no']; ?></td>
-            <td><?php echo $row['subject']; ?></td>
-            <td><?php echo $row['marks']; ?></td>
-            <td><?php echo $row['grade']; ?></td>
-        </tr>
-        <?php } ?>
+        <input type="text" name="roll_no" placeholder="Enter Roll Number" required>
 
-    </table>
+        <input type="text" name="subject" placeholder="Enter Subject" required>
+
+        <input type="number" name="marks" placeholder="Enter Marks" required>
+
+        <input type="text" name="grade" placeholder="Enter Grade" required>
+
+        <button type="submit" name="submit">Add Student</button>
+
+    </form>
+
+    <br>
+
+    <a href="index.php">Back to Home</a>
+
 </div>
 
 </body>
 </html>
+
